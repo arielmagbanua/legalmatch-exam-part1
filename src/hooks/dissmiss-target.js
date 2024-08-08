68f2fdbb11f82b84955d0d84e23516744b69f498
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-08-08 18:49:45 +0800

    Added dissmissing of dropdowns.

---
+++ src/hooks/dissmiss-target.js
@@ -0,0 +1,23 @@
+import {useEffect} from "react";
+
+function useDismissTarget(target, showTarget) {
+  useEffect(() => {
+    const handler = (event) => {
+      if (!target.current) {
+        return; // no reference then nothing else to do
+      }
+
+      if (!target.current.contains(event.target)) {
+        showTarget(false);
+      }
+    }
+
+    document.addEventListener('click', handler, true);
+
+    return () => {
+      document.removeEventListener('click', handler); // during clean-up remove the click listener
+    }
+  }, [target, showTarget]);
+}
+
+export default useDismissTarget;
